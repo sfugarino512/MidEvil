54d7018180b8149fddf168da4ee0315c5fa4ebce
salvatore <sfugarino512@gmail.com>
2013-04-22 15:41:33 -0300

    Create Race.h

---
+++ Race.h
@@ -0,0 +1,49 @@
+/*
+  Filename   : Race.h
+  Author     : Salvatore Fugarino
+  Description: Generic structure of a header file
+*/   
+
+/************************************************************/
+// Macro guard to prevent multiple inclusions
+
+#ifndef RACE_H         
+#define RACE_H
+
+/************************************************************/
+// System includes
+
+#include <string>
+#include <iostream>
+
+/************************************************************/
+// Local includes
+
+/************************************************************/
+// Using declarations
+
+using std::string;
+using std::ostream;
+using std::string;
+
+/************************************************************/
+
+class Race
+{
+public:
+  Race();
+  
+  setRace(string race);
+  
+private:
+
+};
+
+// Place all free function prototypes after the class declaration
+//   Include only functions that act on the class
+
+/************************************************************/
+
+#endif
+
+/************************************************************/
